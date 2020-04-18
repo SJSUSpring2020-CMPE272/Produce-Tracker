082398f2369a883fda16d4c11af7346bf22d7e49
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-04-18 15:26:39 -0700

    integerating the css desigin into react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header class="header">
-        <div class="logo-box">
-          <img alt="Logo" class="logo" />
+    <Router>
+      <React.Fragment>
+        <div className="container jumbo">
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/dashboard" component={Feed} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
         </div>
-        <div class="text-box">
-          <h1 class="heading-primary">
-            <span class="heading-primary-main">Produce Tracker</span>
-            <span class="heading-primary-sub">Secure Storage</span>
-          </h1>
-          <a href="#" class="btn btn-white">
-            Sign In
-          </a>
-        </div>
-      </header>
-    </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.less";
+
+export const Home = () => (
+<header class="header">
+  <div class="header__text-box">
+    <h1 class="heading-primary">
+      <span class="heading-primary--main">Yehia Qtaish</span>
+      <span class="heading-primary--sub">Software Engineer</span>
+    </h1>
+    <a href="#" class="btn btn--white btn--animated">
+      Click Here :)
+    </a>
+  </div>
+</header>;
