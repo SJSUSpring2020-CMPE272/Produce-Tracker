6cd46e9b4fd60e91b79935dcfed7a88b85f815cd
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-04-28 10:56:12 -0700

    re designed dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -3,87 +3,34 @@ import { Link } from "react-router-dom";
 import "./Dashboard.scss";
 
 export const Dashboard = () => (
-  <div id="cd-timeline" class="cd-container">
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-picture">
-			</div>
+<div className="section-dashboard" id="timeline-content">
+  <h1 className="header_h1">Order # Chain Flow</h1>
 
-			<div className="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div className="timeline-content-info">
-          <span className="timeline-content-info-title">
-            <i className="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span className="timeline-content-info-date">
-            <i className="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul className="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
-			</div> 
-		</div> 
-
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-movie">
-			</div>
-
-			<div className="cd-timeline-content">
-				<h2>Title of section 2</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
-				<span className="cd-date">Jan 18</span>
-			</div> 
-		</div> 
-
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-picture">
-			</div> 
-
-			<div className="cd-timeline-content">
-				<h2>Title of section 3</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-				<span className="cd-date">Jan 24</span>
-			</div> 
-		</div> 
-
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-location">
-			</div> 
-
-			<div className="cd-timeline-content">
-				<h2>Title of section 4</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-				<span className="cd-date">Feb 14</span>
-			</div> 
-		</div> 
-
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-location">
-			</div> 
-
-			<div className="cd-timeline-content">
-				<h2>Title of section 5</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
-				<span className="cd-date">Feb 18</span>
-			</div> 
-		</div> 
-
-		<div className="cd-timeline-block">
-			<div className="cd-timeline-img cd-movie">
-			</div> ]
-
-			<div className="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span className="cd-date">Feb 26</span>
-			</div> 
-		</div> 
+  <ul className="timeline">
+    <li className="event" data-date="65Million B.C.">
+      <h3>Dinosaurs Roamed the Earth</h3>
+      <p>RAWWWWWWRRR 🐢🦂</p>
+    </li>
+    <li className="event" data-date="2005">
+      <h3>Creative Component Launched</h3>
+      <p>"We can be all things to all people!" 📣</p>    
+    </li>
+    <li className="event" id="date" data-date="2009">
+      <h3>Squareflair was Born</h3>
+      <p></p> <p>"We can be all things to Squarespace users!" 📣</p>    
+    </li>
+    
+    <li className="event" data-date="November 15, 2018">
+      
+      <h3>Squareflair Today</h3>
+      
+      <p>"We design and build from scratch!" </p> 
+      <p>When we say we mean it— and we build all sites on the Squarespace Developer platform.</p>
+<p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the</p>
+      
+      <p>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</p>
+      
+    </li>
+  </ul>
 </div>
 );
