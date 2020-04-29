42a9f241338709cc6b860276e0a7e26cf00e1280
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-04-28 19:14:33 -0700

    added Axios, and http fetch call

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,35 +2,61 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Dashboard.scss";
 
-export const Dashboard = () => (
-<div className="section-dashboard" id="timeline-content">
-  <h1 className="header_h1">Order # Chain Flow</h1>
+export class Dashboard extends React.Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
 
-  <ul className="timeline">
-    <li className="event" data-date="65Million B.C.">
-      <h3>Dinosaurs Roamed the Earth</h3>
-      <p>RAWWWWWWRRR 🐢🦂</p>
-    </li>
-    <li className="event" data-date="2005">
-      <h3>Creative Component Launched</h3>
-      <p>"We can be all things to all people!" 📣</p>    
-    </li>
-    <li className="event" id="date" data-date="2009">
-      <h3>Squareflair was Born</h3>
-      <p></p> <p>"We can be all things to Squarespace users!" 📣</p>    
-    </li>
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+
+  callApi = async () => {
+    const response = await fetch('/getOrder?key=ORDER1');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
     
-    <li className="event" data-date="November 15, 2018">
-      
-      <h3>Squareflair Today</h3>
-      
-      <p>"We design and build from scratch!" </p> 
-      <p>When we say we mean it— and we build all sites on the Squarespace Developer platform.</p>
-<p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the</p>
-      
-      <p>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</p>
-      
-    </li>
-  </ul>
-</div>
-);
+    return body;
+  };
+
+  render() {
+    return (
+    <div className="section-dashboard" id="timeline-content">
+      <h1 className="header_h1">Order # Chain Flow</h1>
+
+      <ul className="timeline">
+        <li className="event" data-date="65Million B.C.">
+          <h3>Dinosaurs Roamed the Earth</h3>
+          <p>RAWWWWWWRRR 🐢🦂</p>
+        </li>
+        <li className="event" data-date="2005">
+          <h3>Creative Component Launched</h3>
+          <p>"We can be all things to all people!" 📣</p>    
+        </li>
+        <li className="event" id="date" data-date="2009">
+          <h3>Squareflair was Born</h3>
+          <p></p> <p>"We can be all things to Squarespace users!" 📣</p>    
+        </li>
+        
+        <li className="event" data-date="November 15, 2018">
+          
+          <h3>Squareflair Today</h3>
+          
+          <p>"We design and build from scratch!" </p> 
+          <p>When we say we mean it— and we build all sites on the Squarespace Developer platform.</p>
+    <p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the</p>
+          
+          <p>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</p>
+          
+        </li>
+      </ul>
+    </div>
+)
+  }
+
+}
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Signup.scss";
+import { Dashboard } from "./Dashboard";
 
-export const Signup = () => (
+export class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: '',
+      order_details: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
+  render() {
+    return (
   <div class="section-about">
     <div class="u-center-text u-margin-bottom-6">
       <h2 class="heading-secondary">Produce Tracker: Secure Storage!</h2>
@@ -15,8 +39,12 @@ export const Signup = () => (
         <div class="wrapper fadeInDown">
           <div id="formContent">
             <h2 class="active"> Enter Order ID </h2>
-            <form>
-              <input type="text" id="login" class="fadeIn second" />
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" 
+                      id="order" 
+                      class="fadeIn second"
+                      value={this.state.order}
+                      onChange={this.handleChange} />
               <Link to="/dashboard">
               <input type="submit" class="fadeIn fourth" />
               </Link>
@@ -40,4 +68,8 @@ export const Signup = () => (
       </div>
     </div>
   </div>
-);
+    )
+  
+  }
+}
+
